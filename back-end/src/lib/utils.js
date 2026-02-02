b26fb45c5662bdd50aba2a3136c9a696a9125e2f
oommmaarr <omammm61@gmail.com>
2026-02-02 20:00:55 +0200

    final project updates

--- back-end/src/lib/utils.js
+++ back-end/src/lib/utils.js
@@ -8,7 +8,7 @@ const generateToken = (payload , res) => {
     res.cookie("jwt" , token , {
         httpOnly : true,
         secure : process.env.NODE_ENV === "production",
-        sameSite : "lax",
+        sameSite : process.env.NODE_ENV === "production" ? "none" : "lax",
         maxAge : 7 * 24 * 60 * 60 * 1000 // 7 days
     })
     return token;
